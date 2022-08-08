da7a46f5a9e3fcf4d7bac9763fc48f04314f3e8f
alpaycli <98733058+alpaycli@users.noreply.github.com>
2022-08-08 17:23:38 +0400

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alpay Calalli on 02.07.22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
